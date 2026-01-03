c5efcc0c72ff395459a22f4ab6507c14e18521cd
ashwinsuresh-ngr <ashwin2005502@gmail.com>
2026-01-03 22:49:15 +0530

    addition added

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,8 +1,8 @@
 
 
-AUTHOR          = '<your name>'  # use your name
+AUTHOR          = 'Ashwin'  # use your name
 SITENAME        = 'title' # use title
-GITHUB_USERNAME = 'tactlabs' # use your username
+GITHUB_USERNAME = 'ashwinsuresh-ngr' # use your username
 
 
 # Blogroll
@@ -12,8 +12,8 @@ LINKS = (
 
 # Social widget
 SOCIAL = (
-    ("GitHub", "https://github.com/<yourusername>/"),
-    ("LinkedIn", "<linkedin handle>"),
+    ("GitHub", "https://github.com/ashwinsuresh-ngr"),
+    ("LinkedIn", "www.linkedin.com/in/ashwin-s-214a87302"),
 )
 
 ######## Advanced Settings (not recommended to to edited; take your own risk to touch the settings below) #############
